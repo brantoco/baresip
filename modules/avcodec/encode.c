brantoco/baresip

commit 284ecfe3393953c846d1ddfdf417032daf30a84a

Hardcoded: UYVY, /dev/video4, 320x240. New modules: snapshot2, server

modules/avcodec/encode.c
@@ -8,7 +8,9 @@
 #include <baresip.h>
 #include <libavcodec/avcodec.h>
 #include <libavutil/mem.h>
-#ifdef USE_X264
+#if defined(USE_GST_VIDEO)
+#include "gst_video.h"
+#elif defined(USE_X264)
 #include <x264.h>
 #endif
 #include "h26x.h"
@@ -54,7 +56,11 @@ struct videnc_state {
 		} h264;
 	} u;
 
-#ifdef USE_X264
+#if defined(USE_GST_VIDEO)
+	gst_video_t *gst_video;
+	videnc_packet_h *pkth;
+	void *pkth_arg;
+#elif defined(USE_X264)
 	x264_t *x264;
 #endif
 };
@@ -67,7 +73,10 @@ static void destructor(void *arg)
 	mem_deref(st->mb);
 	mem_deref(st->mb_frag);
 
-#ifdef USE_X264
+#if defined(USE_GST_VIDEO)
+	if (st->gst_video)
+		gst_video_free(st->gst_video);
+#elif defined(USE_X264)
 	if (st->x264)
 		x264_encoder_close(st->x264);
 #endif
@@ -250,7 +259,6 @@ int decode_sdpparam_h264(struct videnc_state *st, const struct pl *name,
 	return 0;
 }
 
-
 static void param_handler(const struct pl *name, const struct pl *val,
 			  void *arg)
 {
@@ -331,7 +339,187 @@ static int h263_packetize(struct videnc_state *st, struct mbuf *mb,
 }
 
 
-#ifdef USE_X264
+
+int encode_update(struct videnc_state **vesp, const struct vidcodec *vc,
+		  struct videnc_param *prm, const char *fmtp)
+{
+	struct videnc_state *st;
+	int err = 0;
+
+	if (!vesp || !vc || !prm)
+		return EINVAL;
+
+	if (*vesp)
+		return 0;
+
+	st = mem_zalloc(sizeof(*st), destructor);
+	if (!st)
+		return ENOMEM;
+
+	st->encprm = *prm;
+
+	st->codec_id = avcodec_resolve_codecid(vc->name);
+	if (st->codec_id == CODEC_ID_NONE) {
+		err = EINVAL;
+		goto out;
+	}
+
+	st->mb  = mbuf_alloc(FF_MIN_BUFFER_SIZE * 20);
+	st->mb_frag = mbuf_alloc(1024);
+	if (!st->mb || !st->mb_frag) {
+		err = ENOMEM;
+		goto out;
+	}
+
+	st->sz_max = st->mb->size;
+
+	if (st->codec_id == CODEC_ID_H264) {
+#if !defined(USE_X264) && !defined(USE_GST_VIDEO)
+		err = init_encoder(st);
+#endif
+	}
+	else
+		err = init_encoder(st);
+	if (err) {
+		warning("avcodec: %s: could not init encoder\n", vc->name);
+		goto out;
+	}
+
+	if (str_isset(fmtp)) {
+		struct pl sdp_fmtp;
+
+		pl_set_str(&sdp_fmtp, fmtp);
+
+		fmt_param_apply(&sdp_fmtp, param_handler, st);
+	}
+
+	debug("avcodec: video encoder %s: %d fps, %d bit/s, pktsize=%u\n",
+	      vc->name, prm->fps, prm->bitrate, prm->pktsize);
+
+ out:
+	if (err)
+		mem_deref(st);
+	else
+		*vesp = st;
+
+	return err;
+}
+
+
+#if defined(USE_GST_VIDEO)
+static void gst_pull_callback(void *dst, int size, void *arg)
+{
+	struct videnc_state *st = arg;
+
+	mbuf_reset(st->mb);
+	mbuf_write_mem(st->mb, dst, size); // TODO: delete copying
+
+	/* TODO: Return error codes... somehow. */
+	switch (st->codec_id) {
+
+	case CODEC_ID_H263:
+		h263_packetize(st, st->mb, st->pkth, st->pkth_arg);
+		break;
+
+	case CODEC_ID_H264:
+		h264_packetize(st->mb, st->encprm.pktsize, st->pkth, st->pkth_arg);
+		break;
+
+	case CODEC_ID_MPEG4:
+		general_packetize(st->mb, st->encprm.pktsize, st->pkth, st->pkth_arg);
+		break;
+
+	default:
+		/* err = EPROTO; */
+		break;
+	}
+
+}
+
+int encode_gst(struct videnc_state *st, bool update, const struct vidframe *frame,
+		videnc_packet_h *pkth, void *arg)
+{
+	int err;
+
+	unsigned char *data;
+	int size;
+
+	int width;
+	int height;
+	int fps;
+	int bitrate;
+
+	if (!st || !frame || !pkth)
+		return EINVAL;
+
+	width = frame->size.w;
+	height = frame->size.h;
+	fps = st->encprm.fps;
+	bitrate = st->encprm.bitrate;
+
+	if (!st->gst_video || !vidsz_cmp(&st->encsize, &frame->size))
+	{
+		/* If caps was changed. */
+		if (st->gst_video) {
+			gst_video_free(st->gst_video);
+			st->gst_video = NULL;
+		}
+
+		st->gst_video = gst_video_alloc(width, height, fps, bitrate);
+
+		if (!st->gst_video) {
+			warning("gst_video codec: gst_video_alloc failed\n");
+			return ENOMEM;
+		}
+
+		st->pkth = pkth;
+		st->pkth_arg = arg;
+
+		err = gst_video_set_pull_callback(st->gst_video, gst_pull_callback, (void *)st);
+		if (err) {
+			return err;
+		}
+
+		/* To detect if requested size was changed. */
+		st->encsize = frame->size;
+	}
+
+	if (update) {
+		debug("avcodec: gstreamer picture update, it's not implemented...\n");
+	}
+
+#if 0
+	/* I420 (YUV420P): hardcoded. */
+	size = frame->linesize[0] * height + frame->linesize[1] * height * 0.5 + frame->linesize[2] * height * 0.5;
+
+	data = malloc(size);
+
+	size = 0;
+
+	memcpy(&data[size], frame->data[0], frame->linesize[0] * height);
+	size += frame->linesize[0] * height;
+
+	memcpy(&data[size], frame->data[1], frame->linesize[1] * height * 0.5);
+	size += frame->linesize[1] * height * 0.5;
+
+	memcpy(&data[size], frame->data[2], frame->linesize[2] * height * 0.5);
+	size += frame->linesize[2] * height * 0.5;
+#endif
+
+#if 1
+	/* UYVY: hardcoded. */
+	size = width * height * 2;
+
+	data = malloc(size);
+
+	memcpy(&data[0], frame->data[0], size);
+#endif
+
+	return gst_video_push(st->gst_video, data, size);
+}
+
+#elif defined(USE_X264)
+
 static int open_encoder_x264(struct videnc_state *st, struct videnc_param *prm,
 			     const struct vidsz *size)
 {
@@ -404,76 +592,7 @@ static int open_encoder_x264(struct videnc_state *st, struct videnc_param *prm,
 
 	return 0;
 }
-#endif
 
-
-int encode_update(struct videnc_state **vesp, const struct vidcodec *vc,
-		  struct videnc_param *prm, const char *fmtp)
-{
-	struct videnc_state *st;
-	int err = 0;
-
-	if (!vesp || !vc || !prm)
-		return EINVAL;
-
-	if (*vesp)
-		return 0;
-
-	st = mem_zalloc(sizeof(*st), destructor);
-	if (!st)
-		return ENOMEM;
-
-	st->encprm = *prm;
-
-	st->codec_id = avcodec_resolve_codecid(vc->name);
-	if (st->codec_id == CODEC_ID_NONE) {
-		err = EINVAL;
-		goto out;
-	}
-
-	st->mb  = mbuf_alloc(FF_MIN_BUFFER_SIZE * 20);
-	st->mb_frag = mbuf_alloc(1024);
-	if (!st->mb || !st->mb_frag) {
-		err = ENOMEM;
-		goto out;
-	}
-
-	st->sz_max = st->mb->size;
-
-	if (st->codec_id == CODEC_ID_H264) {
-#ifndef USE_X264
-		err = init_encoder(st);
-#endif
-	}
-	else
-		err = init_encoder(st);
-	if (err) {
-		warning("avcodec: %s: could not init encoder\n", vc->name);
-		goto out;
-	}
-
-	if (str_isset(fmtp)) {
-		struct pl sdp_fmtp;
-
-		pl_set_str(&sdp_fmtp, fmtp);
-
-		fmt_param_apply(&sdp_fmtp, param_handler, st);
-	}
-
-	debug("avcodec: video encoder %s: %d fps, %d bit/s, pktsize=%u\n",
-	      vc->name, prm->fps, prm->bitrate, prm->pktsize);
-
- out:
-	if (err)
-		mem_deref(st);
-	else
-		*vesp = st;
-
-	return err;
-}
-
-
-#ifdef USE_X264
 int encode_x264(struct videnc_state *st, bool update,
 		const struct vidframe *frame,
 		videnc_packet_h *pkth, void *arg)
@@ -547,7 +666,6 @@ int encode_x264(struct videnc_state *st, bool update,
 	return err;
 }
 #endif
-
 
 int encode(struct videnc_state *st, bool update, const struct vidframe *frame,
 	   videnc_packet_h *pkth, void *arg)

src/video.c
@@ -223,7 +223,7 @@ static void encode_rtp_send(struct vtx *vtx, struct vidframe *frame)
 	lock_write_get(vtx->lock);
 
 	/* Convert image */
-	if (frame->fmt != VID_FMT_YUV420P) {
+/*	if (frame->fmt != VID_FMT_YUV420P) {
 
 		vtx->vsrc_size = frame->size;
 
@@ -238,7 +238,7 @@ static void encode_rtp_send(struct vtx *vtx, struct vidframe *frame)
 		vidconv(vtx->frame, frame, 0);
 		frame = vtx->frame;
 	}
-
+*/
 	/* Process video frame through all Video Filters */
 	for (le = vtx->filtl.head; le; le = le->next) {
 

mk/modules.mk
@@ -151,19 +151,19 @@ USE_SYSLOG := $(shell [ -f $(SYSROOT)/include/syslog.h ] || \
 	[ -f $(SYSROOT_ALT)/include/syslog.h ] || \
 	[ -f $(SYSROOT)/local/include/syslog.h ] && echo "yes")
 USE_UUID  := $(shell [ -f $(SYSROOT)/include/uuid/uuid.h ] && echo "yes")
-USE_V4L  := $(shell [ -f $(SYSROOT)/include/libv4l1.h ] || \
-	[ -f $(SYSROOT)/local/include/libv4l1.h ] \
-	&& echo "yes")
+#USE_V4L  := $(shell [ -f $(SYSROOT)/include/libv4l1.h ] || \
+#	[ -f $(SYSROOT)/local/include/libv4l1.h ] \
+#	&& echo "yes")
 USE_V4L2  := $(shell [ -f $(SYSROOT)/include/libv4l2.h ] || \
 	[ -f $(SYSROOT)/local/include/libv4l2.h ] \
 	&& echo "yes")
 USE_X11 := $(shell [ -f $(SYSROOT)/include/X11/Xlib.h ] || \
 	[ -f $(SYSROOT)/local/include/X11/Xlib.h ] || \
 	[ -f $(SYSROOT_ALT)/include/X11/Xlib.h ] && echo "yes")
-USE_VPX  := $(shell [ -f $(SYSROOT)/include/vpx/vp8.h ] \
-	|| [ -f $(SYSROOT)/local/include/vpx/vp8.h ] \
-	|| [ -f $(SYSROOT_ALT)/include/vpx/vp8.h ] \
-	&& echo "yes")
+#USE_VPX  := $(shell [ -f $(SYSROOT)/include/vpx/vp8.h ] \
+#	|| [ -f $(SYSROOT)/local/include/vpx/vp8.h ] \
+#	|| [ -f $(SYSROOT_ALT)/include/vpx/vp8.h ] \
+#	&& echo "yes")
 endif
 
 # Platform specific modules
@@ -347,3 +347,5 @@ endif
 ifneq ($(USE_X11),)
 MODULES   += x11 x11grab
 endif
+
+MODULES   += server snapshot2

modules/avcodec/avcodec.c
@@ -100,7 +100,9 @@ static struct vidcodec h264 = {
 	.name      = "H264",
 	.variant   = "packetization-mode=0",
 	.encupdh   = encode_update,
-#ifdef USE_X264
+#if defined(USE_GST_VIDEO)
+	.ench      = encode_gst,
+#elif defined(USE_X264)
 	.ench      = encode_x264,
 #else
 	.ench      = encode,
@@ -133,10 +135,12 @@ static struct vidcodec mpg4 = {
 
 static int module_init(void)
 {
-#ifdef USE_X264
-	debug("avcodec: x264 build %d\n", X264_BUILD);
+#if defined(USE_GST_VIDEO)
+	info("avcodec: gstreamer\n");
+#elif defined(USE_X264)
+	info("avcodec: x264 build %d\n", X264_BUILD);
 #else
-	debug("avcodec: using FFmpeg H.264 encoder\n");
+	info("avcodec: using FFmpeg H.264 encoder\n");
 #endif
 
 #if LIBAVCODEC_VERSION_INT < ((53<<16)+(10<<8)+0)

modules/avcodec/avcodec.h
@@ -28,6 +28,11 @@ int encode_x264(struct videnc_state *st, bool update,
 		const struct vidframe *frame,
 		videnc_packet_h *pkth, void *arg);
 #endif
+#ifdef USE_GST_VIDEO
+int encode_gst(struct videnc_state *st, bool update,
+	      const struct vidframe *frame,
+	      videnc_packet_h *pkth, void *arg);
+#endif
 
 
 /*

modules/avcodec/gst_video.c
@@ -1,0 +1,369 @@
+/**
+ * @file gst.c  Gstreamer video pipeline
+ *
+ * Copyright (C) 2014 Fadeev Alexander
+ */
+#include <stdlib.h>
+#include <string.h>
+#define __USE_POSIX199309
+#include <sys/time.h>
+#include <pthread.h>
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <gst/app/gstappsrc.h>
+#include "gst_video.h"
+
+
+#define DEBUG_MODULE "gst"
+#define DEBUG_LEVEL 5
+#include <re_dbg.h>
+
+
+/**
+ * Defines the Gstreamer state
+ */
+struct gst_video_state {
+	/* Gstreamer */
+	GstElement *pipeline, *source;
+	GMainLoop *loop;
+
+	/* Main loop thread. */
+	int run;
+	pthread_t tid;
+
+	/* Thread synchronization. */
+	pthread_mutex_t mutex;
+	pthread_cond_t wait;
+	int bwait;
+
+	/* Frame buffer pull callback (send method). */
+	void (*pull)(void *dst, int size, void *arg);
+	void *arg;
+};
+
+static void *internal_thread(void *arg)
+{
+	struct gst_video_state *st = arg;
+
+	/* Now set to playing and iterate. */
+	DEBUG_NOTICE("Setting pipeline to PLAYING\n");
+	gst_element_set_state(st->pipeline, GST_STATE_PLAYING);
+
+	while (st->run) {
+		g_main_loop_run(st->loop);
+	}
+
+	return NULL;
+}
+
+static gboolean internal_bus_watch_handler(GstBus *bus, GstMessage *msg, gpointer data)
+{
+	struct gst_video_state *st = data;
+	GMainLoop *loop = st->loop;
+	GstTagList *tag_list;
+	gchar *title;
+	GError *err;
+	gchar *d;
+
+	(void)bus;
+
+	switch (GST_MESSAGE_TYPE(msg)) {
+
+	case GST_MESSAGE_EOS:
+		DEBUG_NOTICE("End-of-stream\n");
+
+		/* XXX decrementing repeat count? */
+
+		/* Re-start stream */
+		if (st->run) {
+			gst_element_set_state(st->pipeline, GST_STATE_NULL);
+			gst_element_set_state(st->pipeline, GST_STATE_PLAYING);
+		}
+		else {
+			g_main_loop_quit(loop);
+		}
+		break;
+
+	case GST_MESSAGE_ERROR:
+		gst_message_parse_error(msg, &err, &d);
+
+		DEBUG_WARNING("Error: %d(%m) message=%s\n", err->code,
+			      err->code, err->message);
+		DEBUG_WARNING("Debug: %s\n", d);
+
+		g_free(d);
+		g_error_free(err);
+
+		st->run = FALSE;
+		g_main_loop_quit(loop);
+		break;
+
+	case GST_MESSAGE_TAG:
+		gst_message_parse_tag(msg, &tag_list);
+
+		if (gst_tag_list_get_string(tag_list, GST_TAG_TITLE, &title)) {
+			DEBUG_NOTICE("Title: %s\n", title);
+			g_free(title);
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	return TRUE;
+}
+
+static void internal_appsrc_start_feed(GstElement * pipeline, guint size, struct gst_video_state *st)
+{
+	(void)pipeline;
+	(void)size;
+
+	pthread_mutex_lock(&st->mutex);
+	st->bwait = FALSE;
+	pthread_cond_signal(&st->wait);
+	pthread_mutex_unlock(&st->mutex);
+}
+
+static void internal_appsrc_stop_feed(GstElement * pipeline, struct gst_video_state *st)
+{
+	(void)pipeline;
+
+	pthread_mutex_lock(&st->mutex);
+	st->bwait = TRUE;
+	pthread_mutex_unlock(&st->mutex);
+
+}
+
+/* The appsink has received a buffer */
+static void internal_appsink_new_buffer(GstElement *sink, struct gst_video_state *st)
+{
+	GstBuffer *buffer;
+
+	/* Retrieve the buffer */
+	g_signal_emit_by_name(sink, "pull-buffer", &buffer);
+
+
+	if (buffer) {
+		guint8 *data = GST_BUFFER_DATA(buffer);
+		guint size = GST_BUFFER_SIZE(buffer);
+
+		if (st->pull) {
+			st->pull(data, size, st->arg);
+		}
+
+		gst_buffer_unref(buffer);
+	}
+}
+
+/**
+ * Set up the Gstreamer pipeline. Appsrc gets raw frames, and appsink takes
+ * encoded frames.
+ *
+ * The pipeline looks like this:
+ *
+ * <pre>
+ *  .--------.   .-----------.   .----------.
+ *  | appsrc |   |  x264enc  |   | appsink  |
+ *  |   .----|   |----.  .---|   |----.     |
+ *  |   |src |-->|sink|  |src|-->|sink|-----+-->handoff
+ *  |   '----|   |----'  '---|   |----'     |   handler
+ *  '--------'   '-----------'   '----------'
+ * </pre>
+ */
+gst_video_t *gst_video_alloc(int width, int height, int framerate, int bitrate)
+{
+	GstElement *source, *sink;
+	GstBus *bus;
+	GError* gerror = NULL;
+	gchar *version;
+	char pipeline[1024];
+	int err = 0;
+
+	struct gst_video_state *st = malloc(sizeof(struct gst_video_state));
+
+	memset(st, 0, sizeof(struct gst_video_state));
+
+	gst_init(NULL, NULL);
+
+	/* Print gstreamer version. */
+	version = gst_version_string();
+	DEBUG_NOTICE("init: %s\n", version);
+	g_free(version);
+
+#if 0
+	snprintf(pipeline, sizeof(pipeline), "appsrc name=source is-live=TRUE block=TRUE do-timestamp=TRUE ! "
+	                                     "videoparse width=%d height=%d format=i420 framerate=%d/1 ! "
+	                                     "x264enc byte-stream=TRUE rc-lookahead=0 sync-lookahead=0 bitrate=%d ! "
+	                                     "appsink name=sink emit-signals=TRUE drop=TRUE", width, height, framerate, bitrate / 1024 /* kbit/s */);
+
+	DEBUG_NOTICE("format: yu12 = yuv420p = i420\n");
+#endif
+
+#if 1 /* BeagleBoard-xM with camera*/
+/*	snprintf(pipeline, sizeof(pipeline), "v4l2src name=source device=/dev/video2 always-copy=false ! "*/
+	snprintf(pipeline, sizeof(pipeline), "appsrc name=source ! "
+	                                     "video/x-raw-yuv,width=%d,height=%d,format=(fourcc)UYVY,framerate=%d/1 ! "
+	                                     "TIVidenc1 codecName=h264enc engineName=codecServer bitRate=%d ! "
+	                                     "appsink name=sink emit-signals=TRUE drop=TRUE", width, height, framerate, bitrate);
+
+	DEBUG_NOTICE("format: uyvy\n");
+#endif
+
+	DEBUG_NOTICE("width: %d\n", width);
+	DEBUG_NOTICE("height: %d\n", height);
+	DEBUG_NOTICE("framerate: %d\n", framerate);
+	DEBUG_NOTICE("bitrate: %d\n", bitrate);
+
+	/* Initialize pipeline. */
+	st->pipeline = gst_parse_launch(pipeline, &gerror);
+	if (gerror) {
+		DEBUG_WARNING("%s: %s\n", gerror->message, pipeline);
+		err = gerror->code;
+		g_error_free(gerror);
+		goto out;
+	}
+
+	source = gst_bin_get_by_name(GST_BIN(st->pipeline), "source");
+	sink = gst_bin_get_by_name(GST_BIN(st->pipeline), "sink");
+
+	if (!source || !sink) {
+		DEBUG_WARNING("failed to get source or sink pipeline elements");
+		err = ENOMEM;
+		goto out;
+	}
+
+	st->source = source;
+
+#if 1
+	/* Configure appsource*/
+	g_signal_connect(source, "need-data", G_CALLBACK(internal_appsrc_start_feed), st);
+	g_signal_connect(source, "enough-data", G_CALLBACK(internal_appsrc_stop_feed), st);
+#endif
+
+	/* Configure appsink. */
+	g_signal_connect(sink, "new-buffer", G_CALLBACK(internal_appsink_new_buffer), st);
+
+	/********************* Misc **************************/
+
+	/* Bus watch */
+	bus = gst_pipeline_get_bus(GST_PIPELINE(st->pipeline));
+	gst_bus_add_watch(bus, internal_bus_watch_handler, st);
+	gst_object_unref(bus);
+
+	/********************* Thread **************************/
+
+	/* Synchronization primitives. */
+	pthread_mutex_init(&st->mutex, NULL);
+	pthread_cond_init(&st->wait, NULL);
+	st->bwait = FALSE;
+
+	err = gst_element_set_state(st->pipeline, GST_STATE_PLAYING);
+	if (GST_STATE_CHANGE_FAILURE == err) {
+		g_warning("set state returned GST_STATE_CHANGE_FAILUER\n");
+	}
+
+	st->loop = g_main_loop_new(NULL, FALSE);
+
+	/* Launch thread with gstreamer loop. */
+	st->run = TRUE;
+	err = pthread_create(&st->tid, NULL, internal_thread, st);
+	if (err) {
+		st->run = FALSE;
+		goto out;
+	}
+
+out:
+	if (err) {
+		gst_video_free((gst_video_t *)st);
+		st = NULL;
+	}
+
+	return (gst_video_t *)st;
+}
+
+void gst_video_free(gst_video_t *ctx)
+{
+
+	struct gst_video_state *st = (struct gst_video_state *)ctx;
+
+	if (st) {
+		/* Stop thread. */
+		if (st->run) {
+			st->run = FALSE;
+			g_main_loop_quit(st->loop);
+			pthread_join(st->tid, NULL);
+		}
+
+		gst_element_set_state(st->pipeline, GST_STATE_NULL);
+		gst_object_unref(GST_OBJECT(st->pipeline));
+
+		free(st);
+	}
+}
+
+int gst_video_push(gst_video_t *ctx, const void *src, int size)
+{
+	struct gst_video_state *st = (struct gst_video_state *)ctx;
+
+	GstBuffer *buffer;
+	int ret = 0;
+
+	if (!st) {
+		return EINVAL;
+	}
+
+	if (!size) {
+		DEBUG_WARNING("eos returned %d at %d\n", ret, __LINE__);
+		gst_app_src_end_of_stream((GstAppSrc *)st->source);
+		return ret;
+	}
+
+	/* Wait "start feed". */
+	pthread_mutex_lock(&st->mutex);
+	if (st->bwait) {
+#define WAIT_TIME_SECONDS 5
+		struct timespec ts;
+		struct timeval tp;
+		gettimeofday(&tp, NULL);
+		ts.tv_sec  = tp.tv_sec;
+		ts.tv_nsec = tp.tv_usec * 1000;
+		ts.tv_sec += WAIT_TIME_SECONDS;
+		/* Wait. */
+		ret = pthread_cond_timedwait(&st->wait, &st->mutex, &ts);
+		if (ETIMEDOUT == ret) {
+			DEBUG_WARNING("Raw frame is lost because of timeout\n");
+			return ret;
+		}
+	}
+	pthread_mutex_unlock(&st->mutex);
+
+	/* Create a new empty buffer */
+	buffer = gst_buffer_new();
+	GST_BUFFER_MALLOCDATA(buffer) = (guint8 *)src;
+	GST_BUFFER_SIZE(buffer) = (guint)size;
+	GST_BUFFER_DATA(buffer) = GST_BUFFER_MALLOCDATA(buffer);
+
+	ret = gst_app_src_push_buffer((GstAppSrc *)st->source, buffer);
+
+	if (ret != GST_FLOW_OK) {
+		DEBUG_WARNING("push buffer returned %d for %d bytes \n", ret, size);
+		return ret;
+	}
+
+	return ret;
+}
+
+int gst_video_set_pull_callback(gst_video_t *ctx, void (*f)(void *dst, int size, void *arg), void *arg)
+{
+	struct gst_video_state *st = (struct gst_video_state *)ctx;
+
+	if (!st) {
+		return EINVAL;
+	}
+
+	st->pull = f;
+	st->arg = arg;
+
+	return 0;
+}

modules/avcodec/gst_video.h
@@ -1,0 +1,13 @@
+/**
+ * @file gst.h  Gstreamer video pipeline
+ *
+ * Copyright (C) 2014 Fadeev Alexander
+ */
+
+typedef struct { int dummy; } gst_video_t;
+
+gst_video_t *gst_video_alloc(int width, int height, int framerate, int bitrate);
+void gst_video_free(gst_video_t *ctx);
+
+int gst_video_push(gst_video_t *ctx, const void *src, int size);
+int gst_video_set_pull_callback(gst_video_t *ctx, void (*f)(void *dst, int size, void *arg), void *arg);

modules/avcodec/module.mk
@@ -8,12 +8,19 @@ USE_X264 := $(shell [ -f $(SYSROOT)/include/x264.h ] || \
 	[ -f $(SYSROOT)/local/include/x264.h ] || \
 	[ -f $(SYSROOT_ALT)/include/x264.h ] && echo "yes")
 
+USE_GST_VIDEO := $(shell [ "$(USE_GST)" = "yes" ] && echo "yes")
+
 MOD		:= avcodec
 $(MOD)_SRCS	+= avcodec.c h263.c h264.c encode.c decode.c
 $(MOD)_LFLAGS	+= -lavcodec -lavutil
 CFLAGS          += -I/usr/include/ffmpeg
+ifneq ($(USE_GST_VIDEO),)
+$(MOD)_SRCS	+= gst_video.c
+$(MOD)_LFLAGS	+= `pkg-config --libs gstreamer-0.10 gstreamer-app-0.10`
+CFLAGS          += -DUSE_GST_VIDEO `pkg-config --cflags gstreamer-0.10 gstreamer-app-0.10`
+endif
 ifneq ($(USE_X264),)
-CFLAGS          += -DUSE_X264
+CFLAGS	+= -DUSE_X264
 $(MOD)_LFLAGS	+= -lx264
 endif
 

commit 9045b7f872042d5dee3bfc3b00ef41203de58ade

Video was broken on PC after 284ecfe3393953c846d1ddfdf417032daf30a84a

modules/avcodec/encode.c
@@ -505,7 +505,7 @@ int encode_gst(struct videnc_state *st, bool update, const struct vidframe *fram
 		debug("avcodec: gstreamer picture update, it's not implemented...\n");
 	}
 
-#if 0
+#ifndef TARGET_BRANTO
 	/* I420 (YUV420P): hardcoded. */
 	size = frame->linesize[0] * st->height + frame->linesize[1] * st->height * 0.5 + frame->linesize[2] * st->height * 0.5;
 
@@ -521,9 +521,7 @@ int encode_gst(struct videnc_state *st, bool update, const struct vidframe *fram
 
 	memcpy(&data[size], frame->data[2], frame->linesize[2] * st->height * 0.5);
 	size += frame->linesize[2] * st->height * 0.5;
-#endif
-
-#if 1
+#else
 	/* UYVY: hardcoded. */
 	size = st->width * st->height * 2;
 

src/video.c
@@ -222,13 +222,13 @@ static void encode_rtp_send(struct vtx *vtx, struct vidframe *frame)
 
 	lock_write_get(vtx->lock);
 
+#ifndef TARGET_BRANTO
 	/* Convert image */
 	if (frame->fmt != VID_FMT_INTERNAL) {
 
 		vtx->vsrc_size = frame->size;
 
 		if (!vtx->frame) {
-
 			err = vidframe_alloc(&vtx->frame, VID_FMT_INTERNAL,
 					     &vtx->vsrc_size);
 			if (err)
@@ -238,6 +238,7 @@ static void encode_rtp_send(struct vtx *vtx, struct vidframe *frame)
 		vidconv(vtx->frame, frame, 0);
 		frame = vtx->frame;
 	}
+#endif
 
 	/* Process video frame through all Video Filters */
 	for (le = vtx->filtl.head; le; le = le->next) {